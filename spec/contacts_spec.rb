2b306ddec96981d1297e19c4c9ef4ab815e7ff69
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-05-26 15:34:43 -0400

    Update readme and spec to use ice_cream instead of icecream

--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -2,24 +2,24 @@ require_relative 'spec_helper'
 require_relative '../lib/contacts.rb'
 
 
-describe "#remove_strawberry" do 
-  let :contacts do 
+describe "#remove_strawberry" do
+  let :contacts do
     {
       "Jon Snow" => {
         name: "Jon",
-        email: "jon_snow@thewall.we", 
-        favorite_icecream_flavors: ["chocolate", "vanilla"]
+        email: "jon_snow@thewall.we",
+        favorite_ice_cream_flavors: ["chocolate", "vanilla"]
       },
       "Freddy Mercury" => {
         name: "Freddy",
         email: "freddy@mercury.com",
-        favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+        favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
       }
     }
   end
 
-  it "removes the strawberry ice cream from :favorite_icecream_flavors" do 
+  it "removes the strawberry ice cream from :favorite_ice_cream_flavors" do
     result = remove_strawberry(contacts)
-    expect(result["Freddy Mercury"][:favorite_icecream_flavors]).to_not include("strawberry")
+    expect(result["Freddy Mercury"][:favorite_ice_cream_flavors]).to_not include("strawberry")
   end
 end
