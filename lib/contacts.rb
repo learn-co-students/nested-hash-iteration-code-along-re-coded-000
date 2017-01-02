915edc597be2e1202b18c82a68160be34b4dc86c
Ali Diyab <alidiyab1997@gmail.com>
2017-01-02 17:48:58 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,19 +1,26 @@
 require 'pry'
 
   # This is the array we will be passing into the remove_strawberry method
-  # contacts = {
-  #   "Jon Snow" => {
-  #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_icecream_flavors: ["chocolate", "vanilla"]
-  #   },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
-  
+   contacts = {
+     "Jon Snow" => {
+       name: "Jon",
+       email: "jon_snow@thewall.we",
+       favorite_icecream_flavors: ["chocolate", "vanilla"]
+     },
+     "Freddy Mercury" => {
+       name: "Freddy",
+       email: "freddy@mercury.com",
+       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+   }
+}
 def remove_strawberry(contacts)
-
+contacts.each do |person, data|
+person.each do |attribute, value|
+  if attribute == :favorite_icecream_flavors
+    value.delete_if do |ice_cream|
+      ice_cream = "strawberry"
+  end
+end
+end
+end
 end
