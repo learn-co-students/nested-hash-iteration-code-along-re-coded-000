ec98dde0a64839acc46a145816daed3d823a9c23
dina aziz <dina.aziz100@yahoo.com>
2017-01-10 22:14:40 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,10 +15,10 @@ require 'pry'
   # }
 
   def remove_strawberry(contacts)
-    contacts.each do |person, contact_details_hash|
-      contact_details_hash.each do |attribute, data|
-        if attribute == :favorite_icecream_flavors
-          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+    contacts.each do |keys, values|
+    values.each do |key, value|
+        if key == :favorite_icecream_flavors
+          value.delete_if {|ice_cream| ice_cream == "strawberry"}
         end
       end
     end
